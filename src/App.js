8ed0764ef3b2ca9f78a3c217d66ab56d3461d893
Eduardo Ferreira <eduardo.ferreira1983@gmail.com>
2021-10-18 12:57:36 -0300

    alteraç ões 18/10

--- src/App.js
+++ src/App.js
@@ -5,14 +5,46 @@ import './App.css';
 // import Main from './components/main';
  import Exemplo from './components/exemplostate' //classes
 // import ExemploHooks from './components/exemplostatehooks'
+// import Card from './components/cardlist-class'
+import Card from './components/cardlist-hooks'
+import { useEffect, useState } from 'react'
+import User from './components/user'
+
+function App()
+{
+
+  // const monsters = [
+  //   {name: 'Frankstein', id:'1'},
+  //   {name: 'Vampire', id:'2'},
+  //   {name: 'Zombie', id:'3'},
+  //   {name: 'Bozoloide', id:'4'},
+  // ]
+
+  const [ users, setUsers ] = useState();
+
+  // const number = 666;
+
+  // useEffect(() => {
+  //   // Atualiza o titulo do documento usando a API do browser
+  //   fetch( 'https://jsonplaceholder.typicode.com/users' )
+  //     .then( response => response.json() ) // formatar para json
+  //     .then (users=> console.log(users) ) // saida do array - > 
+  // } );
+  
+  useEffect( () =>
+  {
+    // Atualiza o titulo do documento usando a API do browser
+    fetch( 'https://jsonplaceholder.typicode.com/users' )
+      .then( response => response.json() ) // formatar para json -> response.json()
+      .then( usr => setUsers( usr ) ) // saida do array - > 
+  },[]);
+  
 
 
-
-function App()  {
   return ( // é o que será renderizado no  browser
     
     <div >
-      <Exemplo/>
+      <User/>
     </div>
   );
 }
